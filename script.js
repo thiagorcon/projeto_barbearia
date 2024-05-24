741b95bd8354ac995d4ac42cbc8c7c551bbc9e9c
Thiago Rosa <thiagorcon@hotmail.com>
2024-05-24 17:23:09 -0300

    envio dos codigos

--- script.js
+++ script.js
@@ -19,27 +19,110 @@ const barbearia = {
   
   
 function buscaCortePorId(id) {
+    for (let corte of barbearia.cortes) {
+        
+        if (corte.id === id) {
+            
+            return corte;
+        }
+    }
+    
+    return 'Corte não encontrado'
 }
 
 function buscaBarbaPorId(id) {
+    for (let barba of barbearia.barbas) {
+        
+        if (barba.id === id) {
+            
+            return barba;
+        }
+    }
+    
+    return 'Barba não encontrada'
+
 }
 
 function verificaStatusBarbearia() {
+    
+if (barbearia.estaAberto == true) {
+    return 'Estamos abertos'
+
+}
+return 'Estamos fechados'
+
+    
 }
 
 function retornaTodosCortes() {
+    
+
+let listaDeCortes = [];
+
+
+for (let i = 0; i < barbearia.cortes.length; i++) {
+
+    listaDeCortes.push(barbearia.cortes[i]);
 }
+
+
+return listaDeCortes;
+    
+}
+
 
 function retornaTodasBarbas() {
+
+    let listaDeBarbas = [];
+
+
+for (let i = 0; i < barbearia.barbas.length; i++) {
+
+    listaDeBarbas.push(barbearia.barbas[i]);
 }
 
-function criaPedido(nomeCliente, corteId, barbaId) {
+
+return listaDeBarbas;
+
 }
+
+function criaPedido(nome, idCorte, idBarba) {
+    const corte = buscaCortePorId(idCorte);
+    const barba = buscaBarbaPorId(idBarba);
+
+    const pedido = {
+        nome: nome,
+        pedidoCorte: corte.tipo,
+        pedidoCortePreco: corte.valor,
+        pedidoBarba: barba.tipo,
+        pedidoBarbaPreco: barba.valor,
+    };
+    
+    return pedido;
+}
+
 
 function atualizarServico(lista, id, valor, tipo) {
+    
+   
+   for (let i = 0; i < lista.length; i++) {
+    
+    if (lista[i].id === id) {
+        
+        lista[i].tipo = tipo;
+        lista[i].valor = valor;
+       // break; 
+    }
+}
+
+return lista;
 }
 
 function calculaTotal(pedido) {
+    
+    const total = pedido.pedidoCortePreco + pedido.pedidoBarbaPreco;
+    
+    return total;
 }
   
   
